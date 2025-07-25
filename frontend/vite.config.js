ea1b4dff3789d85fcc6ff5aaf03328d766bbb077
Muhammad Zia Un Nabi  Janjua <88709441+iamrlz@users.noreply.github.com>
2025-07-25 02:46:00 +0000

    fix github pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'https://github.com/iamrlz/prize-bond-checker',  // 👈 This is required for GitHub Pages
+  base: '/prize-bond-checker/',
   plugins: [react()],
 });
